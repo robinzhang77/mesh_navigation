ce8290b5e1dc3c21753aa40a24de8f42b2793bda
zhangxiaobin <eway365@qq.com>
2021-04-18 15:02:27 +0800

    代码整理

--- navimeshtest/Form1.cs
+++ navimeshtest/Form1.cs
@@ -13,6 +13,7 @@ namespace navimeshtest
 {
     public partial class Form1 : Form
     {
+        meshnavigation meshnav = new meshnavigation();
         public Form1()
         {
             InitializeComponent();
@@ -20,95 +21,30 @@ namespace navimeshtest
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
 
-        private int ReadInt(FileStream fs)
-        {
-            byte[] bs = new byte[4];
-            fs.Read(bs, 0, sizeof(int));
-            fs.Seek(sizeof(int), SeekOrigin.Current);
-            return System.BitConverter.ToInt32(bs, 0);
-        }
-
-        private float ReadFloat(FileStream fs)
-        {
-            byte[] bs = new byte[4];
-            fs.Read(bs, 0, sizeof(float));
-            fs.Seek(sizeof(float), SeekOrigin.Current);
-            return System.BitConverter.ToSingle(bs, 0);
-        }
-
-        class Vector3
-        {
-            public float x;
-            public float y;
-            public float z;
-        }
-
-        List<int> indices = new List<int>();
-        Dictionary<int, Vector3> vectexts = new Dictionary<int, Vector3>();
-        float fMinX = .0f;
-        float fMinZ = .0f;
-        float fMaxX = .0f;
-        float fMaxZ = .0f;
-        float meshWith = .0f;
-        float meshHeight = .0f;
         private void 打开ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            string strFilepath = "D:/work/UnityProjects/navmesh/Assets/mapdata.data";
-            FileStream fs = new FileStream(strFilepath, FileMode.Open);
-            if (fs == null)
-            {
-                return;
-            }
-
-            fs.Seek(0, SeekOrigin.Begin);
-            int nIndexNum = ReadInt(fs);
-            int nVectextNum = ReadInt(fs);
-
-            for (int i=0; i<nIndexNum; i++)
-            {
-                int nVal = ReadInt(fs);
-                indices.Add(nVal);
-            }
-
-            
-            for (int i=0; i<nVectextNum; i++)
-            {
-                Vector3 v = new Vector3();
-                v.x = ReadFloat(fs);
-                v.y = ReadFloat(fs);
-                v.z = ReadFloat(fs);
-                vectexts[i] = v;
-                if (v.x < fMinX) fMinX = v.x;
-                if (v.z < fMinZ) fMinZ = v.z;
-                if (v.x > fMaxX) fMaxX = v.x;
-                if (v.z > fMaxZ) fMaxZ = v.z;
-            }
-            meshWith = (fMaxX - fMinX);
-            meshHeight = (fMaxZ - fMinZ);
-
-            fMinX = Math.Abs(fMinX) + 50;
-            fMinZ = Math.Abs(fMinZ) + 50;
-
-            fs.Close();
+            //string strFilepath = "D:/work/UnityProjects/navmesh/Assets/mapdata.data";
+            string strFilepath = "./mapdata.data";
+            meshnav.LoadMeshData(strFilepath);
             this.Refresh();
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            g.Clear(Color.White);
-            
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            g.Clear(Color.Black);
 
-            for (int i = 0; i < indices.Count();)
+            for (int i = 0; i < meshnav.indices.Count();)
             {
-                Vector3 v1 = vectexts[indices[i]];
+                Vector3 v1 = meshnav.vectexts[meshnav.indices[i]];
                 i++;
-                Vector3 v2 = vectexts[indices[i]];
+                Vector3 v2 = meshnav.vectexts[meshnav.indices[i]];
                 i++;
-                Vector3 v3 = vectexts[indices[i]];
+                Vector3 v3 = meshnav.vectexts[meshnav.indices[i]];
                 i++;
                 DrawTriangle_1(g, v1, v2, v3);
             }
@@ -121,8 +57,8 @@ namespace navimeshtest
             int ClientHeight = this.Height;
             float fWidthOffset = ClientWidth / 2;
             float fHeightOffset = ClientHeight / 2;
-            float xScale = (this.Width) / meshWith;
-            float zScale = (this.Height) / meshHeight;
+            float xScale = (this.Width) / meshnav.meshWith;
+            float zScale = (this.Height) / meshnav.meshHeight;
             float fScale = Math.Min(xScale, zScale);
             fScale = fScale * 0.6f;
             //fWidthOffset = fHeightOffset = 0;
---
+++ navimeshtest/meshnavigation.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace navimeshtest
+{
+    class Vector3
+    {
+        public float x;
+        public float y;
+        public float z;
+    }
+
+    class meshnavigation
+    {
+        public List<int> indices = new List<int>();
+        public Dictionary<int, Vector3> vectexts = new Dictionary<int, Vector3>();
+        public float meshWith = .0f;
+        public float meshHeight = .0f;
+
+        public void LoadMeshData(string strFilepath)
+        {
+            FileStream fs = new FileStream(strFilepath, FileMode.Open);
+            if (fs == null)
+            {
+                return;
+            }
+
+            fs.Seek(0, SeekOrigin.Begin);
+            int nIndexNum = ReadInt(fs);
+            int nVectextNum = ReadInt(fs);
+
+            for (int i = 0; i < nIndexNum; i++)
+            {
+                int nVal = ReadInt(fs);
+                indices.Add(nVal);
+            }
+
+            float fMinX = .0f;
+            float fMinZ = .0f;
+            float fMaxX = .0f;
+            float fMaxZ = .0f;
+
+            for (int i = 0; i < nVectextNum; i++)
+            {
+                Vector3 v = new Vector3();
+                v.x = ReadFloat(fs);
+                v.y = ReadFloat(fs);
+                v.z = ReadFloat(fs);
+                vectexts[i] = v;
+                if (v.x < fMinX) fMinX = v.x;
+                if (v.z < fMinZ) fMinZ = v.z;
+                if (v.x > fMaxX) fMaxX = v.x;
+                if (v.z > fMaxZ) fMaxZ = v.z;
+            }
+            meshWith = (fMaxX - fMinX);
+            meshHeight = (fMaxZ - fMinZ);
+            fs.Close();
+        }
+
+        private int ReadInt(FileStream fs)
+        {
+            byte[] bs = new byte[4];
+            fs.Read(bs, 0, sizeof(int));
+            fs.Seek(sizeof(int), SeekOrigin.Current);
+            return System.BitConverter.ToInt32(bs, 0);
+        }
+
+        private float ReadFloat(FileStream fs)
+        {
+            byte[] bs = new byte[4];
+            fs.Read(bs, 0, sizeof(float));
+            fs.Seek(sizeof(float), SeekOrigin.Current);
+            return System.BitConverter.ToSingle(bs, 0);
+        }
+
+        private void DuplicateRemovalIndices()
+        {
+
+        }
+    }
+}
